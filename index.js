3dfbf1b942ccaa2c442d75d5c2c2f4f54f3cd979
Asadsheblu <shebluasad@gmail.com>
2022-04-30 20:24:35 +0600

    deleted-add

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
          res.send(result)
          
      })
+     //delete specific item
+     app.delete('/inventory/:id',async(req,res)=>{
+         const id=req.params.id;
+         const query={_id:ObjectId(id)}
+         const result = await Productcollection.deleteOne(query);
+         res.send(result)
+
+     })
 
     }
     finally{
