4bd9ee21f5a5b2ed3722e93dd7976ebbc95ce034
Asadsheblu <shebluasad@gmail.com>
2022-05-01 12:03:02 +0600

    check-jwt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.get('/',(req,res)=>{
-    res.send("HelloMahira furiture backend sever")
+    res.send("Hello Mahira furiture backend sever")
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7auxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
