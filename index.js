4ef7c1db6648ecdf3b1d1dd0ab0461c4a4ea7e24
Asadsheblu <shebluasad@gmail.com>
2022-05-03 06:35:38 +0600

    port-updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express')
 
-const port = process.env.PORT;
+const port = process.env.PORT ||5000;
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("HelloMahira furiture backend sever")
